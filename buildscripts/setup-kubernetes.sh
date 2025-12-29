ee8f0bddb650492c9ad279bab768211386b8910a
Raj Chaudhuri <rajch@hotmail.com>
2025-12-29 13:50:56 +0530

    Updated for building K8s 1.34 by default, and filesystem is not ext4

--- buildscripts/setup-kubernetes.sh
+++ buildscripts/setup-kubernetes.sh
@@ -24,6 +24,9 @@ case "${KUBE_VERSION}" in
   "1.31" | "1.32" | "1.33")
     SANDBOX_IMAGE_VERSION="3.10"
     ;;
+  "1.34")
+    SANDBOX_IMAGE_VERSION="3.10.1"
+    ;;
   *)
     SANDBOX_IMAGE_VERSION=""
     ;;
